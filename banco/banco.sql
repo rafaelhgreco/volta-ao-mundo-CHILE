e99e0ae7afa2f72ae5eee0b916ce90d15ef68b82
rafaelhgreco <zrafaelgreco@gmail.com>
2024-06-02 23:51:06 -0300

    banco

--- banco/banco.sql
+++ banco/banco.sql
@@ -5,7 +5,7 @@ USE comments_system;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULLg
 );
 
 CREATE TABLE comments (
