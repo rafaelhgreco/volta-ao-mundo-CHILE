313a9893542455bb34d0512ce278421082dcae30
rafaelhgreco <zrafaelgreco@gmail.com>
2024-06-02 23:48:57 -0300

    banco

--- banco/banco.sql
+++ banco/banco.sql
@@ -6,7 +6,6 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    is_admin BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE comments (
