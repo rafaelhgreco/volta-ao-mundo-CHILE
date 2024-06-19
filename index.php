dc1f73575613fbe743a8be3d0419ba22e757ab6f
rafaelhgreco <zrafaelgreco@gmail.com>
2024-06-19 13:24:47 -0300

    Melhorias header

--- index.php
+++ index.php
@@ -23,10 +23,10 @@ function format_date_pt_br($date) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Chile: Turismo, Cultura e Gastronomia</title>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/custom.css" rel="stylesheet">
+  <link rel="stylesheet" href="css.css">
 </head>
 <body>
-  <header>
+  <header id="header">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a style="color:red" class="navbar-brand" href="index.php"><img style="width: 35px;margin-bottom: 4px;" src="img/cl.jpg"><label style="margin-left: 10px;">Chile</label></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -51,25 +51,26 @@ function format_date_pt_br($date) {
       <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="d-block w-100" src="img/turismo/local1.jpg" alt="Turismo no Chile">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Turismo no Chile</h5>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="img/cultura/evento1.jpg" alt="Cultura Chilena">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Cultura Chilena</h5>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="img/gastronomia/prato1.jpg" alt="Gastronomia Chilena">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Gastronomia Chilena</h5>
-        </div>
-      </div>
+  <div class="carousel-item active">
+    <img class="d-block w-100" src="img/turismo/local1.jpg" alt="Turismo no Chile">
+    <div class="carousel-caption d-none d-md-block">
+      <h5 class="highlight-text">Turismo no Chile</h5>
     </div>
+  </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src="img/cultura/evento1.jpg" alt="Cultura Chilena">
+    <div class="carousel-caption d-none d-md-block">
+      <h5 class="highlight-text">Cultura Chilena</h5>
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src="img/gastronomia/prato1.jpg" alt="Gastronomia Chilena">
+    <div class="carousel-caption d-none d-md-block">
+      <h5 class="highlight-text">Gastronomia Chilena</h5>
+    </div>
+  </div>
+</div>
+
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
@@ -192,6 +193,16 @@ function format_date_pt_br($date) {
         }
       });
     });
+    window.addEventListener('scroll', function() {
+    const header = document.getElementById('header');
+    const sticky = header.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
   </script>
 </body>
 </html>
